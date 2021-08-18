ea7accf6dbf84030df6a78086f3d42e5ecd963b3
seiruad <serryadserryad@gmail.com>
2021-08-19 00:51:12 +0700

    dynamic filter options
    add: filter section params generated dynamically

---
+++ src/app/services/table.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { TABLE } from '../api/mock-api';
+import { Table } from '../interfaces/table';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TableService {
+
+  constructor() { }
+
+  getTable(): Observable<Table[]> {
+    const table = of(TABLE) 
+    return table
+  }
+}
