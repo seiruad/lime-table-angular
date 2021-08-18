bc0d9e03275ebda0e9845fc972ffb7279d569ef6
seiruad <serryadserryad@gmail.com>
2021-08-19 00:39:22 +0700

    dynamic group options
    add: group options dynamically generates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableComponent } from './components/table/table.component';
+import { FilterComponent } from './components/filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent
+    TableComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TABLE } from 'src/app/api/mock-api';
+import { makeGroups } from 'src/app/helpers/filterHelpers';
 import { Table } from 'src/app/interfaces/table';
+import { TableService } from 'src/app/services/table.service';
 
 @Component({
   selector: 'app-table',
@@ -9,11 +11,32 @@ import { Table } from 'src/app/interfaces/table';
 })
 export class TableComponent implements OnInit {
   table: Table[] = []
+  groups: any = []
 
+  constructor(private tableService: TableService) { 
+    
+    // this.groups = [{
+    //   name: 'Пол',
+    //   params: [{
+    //     value: 'female',
+    //     count: 1
+    //   }, {
+    //     value: 'male',
+    //     count: 2
+    //   }]
 
-  constructor() { }
+    // }]
+  }
 
   ngOnInit(): void {
-    this.table = TABLE
+    this.getTable()
+    this.groups = makeGroups(this.table)
+    // console.log({group: this.groups})
+    // console.log({groupParams: this.groups[0].params})
+  }
+
+  getTable(): void {
+    this.tableService.getTable()
+      .subscribe(table => this.table = table)
   }
 }
